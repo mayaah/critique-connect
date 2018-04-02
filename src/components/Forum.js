6e27f3f069d929a50da87b4a3cb15785c132686d
Maya Angelica Hernandez <mayaah@berkeley.edu>
2018-04-01 23:58:10 -0400

    Forum threads kind of work

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -3,13 +3,61 @@ import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom';
 import { Grid, Row, Col, Image, Button, Tooltip, OverlayTrigger, Label } from 'react-bootstrap';
 import { Checkbox, TextArea, RadioGroup, Radio } from "@blueprintjs/core";
 
+import { firebaseDB, base } from '../base'
+
 class Forum extends Component {
 	constructor() {
     super()
       this.state = {
       redirect: false,
+      threads: []
     }
+
+    this.threadsRef = firebaseDB.database().ref(`/Threads`)
+    this.simplifyDate = this.simplifyDate.bind(this);
   }	
+
+  componentWillMount() {
+  	this.threadsRef.on('value', snapshot => {
+      let threads = snapshot.val();
+      let newState = [];
+      let promises = [];
+      for (let thread in threads) {
+      	var threadRef = firebaseDB.database().ref(`/Threads/${thread}`)
+      	promises.push(threadRef.once('value')); 
+      }
+      Promise.all(promises).then((snapshots) => {
+        snapshots.forEach((snapshot) => {
+          var thread = snapshot.val()
+	        newState.push({
+	          id: snapshot.key,
+	          author: thread.author,
+	          date: this.simplifyDate(new Date(thread.date).toUTCString()),
+	          topic: thread.topic,
+	          postsCount: Object.keys(thread.Posts).length
+	        });
+	        var sortedArr = newState.sort(function(a, b){
+	          var keyA = new Date(a.date),
+	              keyB = new Date(b.date);
+	          // Compare the 2 dates
+	          if(keyA < keyB) return 1;
+	          if(keyA > keyB) return -1;
+	          return 0;
+	        });
+	        console.log(sortedArr)
+	        this.setState({
+	          threads: sortedArr
+	        });
+	      });
+      })
+    })
+  }
+
+  simplifyDate(date) {
+    let dateArray = date.split(" ")
+    let dateOnly = dateArray.slice(1, 4)
+    return dateOnly.join(" ")
+  }
 
   render() {
 
@@ -22,6 +70,25 @@ class Forum extends Component {
 		      		<Link className="flex" to={"/submit_thread"} >New Thread</Link>
 		      	</Button>
 		      </Col>
+	      </Row>
+	      <Row className="forum-grid">
+	      	<Col sm={12}>
+	      		{this.state.threads.map((thread) => {
+              return (
+              	<Row className="forum-thread-row">
+              		<Col sm={8}>
+              			<div className="thread-row-topic">{thread.topic}</div>
+              		</Col>
+              		<Col sm={2}>
+              			<div className="thread-row-postCount">{thread.postsCount} comments</div>
+              		</Col>
+              		<Col sm={2}>
+              			<div className="thread-row-date">{thread.date}</div>
+              		</Col>
+              	</Row>
+              )
+            })}
+	      	</Col>
 	      </Row>
       </Grid>
 	  );
