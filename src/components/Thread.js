1aa404d9611daa094856a3fc14865fbc9e9e5b40
Maya Angelica Hernandez <mayaah@berkeley.edu>
2018-04-07 18:30:16 -0400

    Omg forgot to add the thread

---
+++ src/components/Thread.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Redirect } from 'react-router-dom';
+import NewPostForm from './NewPostForm'
+import { Grid, Row, Col, Image, Button, Tooltip, OverlayTrigger, Label } from 'react-bootstrap';
+
+import { firebaseDB, base } from '../base'
+
+
+class Thread extends Component {
+	constructor(props){
+    super(props)
+    this.state = {
+      redirect: false,
+      threadId: this.props.match.params.threadId,
+      topic: "",
+      posts: []
+    }
+    this.threadRef = firebaseDB.database().ref(`Threads/${this.state.threadId}`);
+    this.postsRef = firebaseDB.database().ref(`Threads/${this.state.threadId}/Posts`)
+    this.simplifyDate = this.simplifyDate.bind(this);
+  }
+
+  componentDidMount() {
+    this.threadRef.on('value', snapshot => {
+      let thread = snapshot.val()
+      this.setState({
+        topic: thread.topic ? thread.topic : "",
+      });
+    })
+    this.postsRef.on('value', snapshot => {
+      let posts = snapshot.val();
+      let newState = [];
+      let promises = [];
+      for (let post in posts) {
+        var postRef = firebaseDB.database().ref(`/Posts/${post}`)
+        promises.push(postRef.once('value')); 
+      }
+      Promise.all(promises).then((snapshots) => {
+        snapshots.forEach((snapshot) => {
+          var post = snapshot.val()
+          let postAuthorName = ""
+          let postAuthorAvatar = ""
+          let postAuthorId = ""
+          var postAuthorRef = firebaseDB.database().ref(`/Users/${post.author}`)
+          postAuthorRef.once('value', snapshot2 => {
+            let postAuthor = snapshot2.val();
+            postAuthorName = postAuthor.displayName,
+            postAuthorAvatar = postAuthor.avatarURL
+            newState.push({
+              id: snapshot.key,
+              authorId: post.author,
+              author: postAuthorName,
+              authorAvatar: postAuthorAvatar,
+              comment: post.comment,
+              date: post.date
+            });
+            var sortedArr = newState.sort(function(a, b){
+              var keyA = new Date(a.date),
+                  keyB = new Date(b.date);
+              // Compare the 2 dates
+              if(keyA < keyB) return -1;
+              if(keyA > keyB) return 1;
+              return 0;
+            });
+            console.log(sortedArr)
+            this.setState({
+              posts: sortedArr
+            });
+          })
+        });
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.threadRef.off()
+    this.postsRef.off()
+  }
+
+  simplifyDate(date) {
+    let dateArray = date.split(" ")
+    let dateOnly = dateArray.slice(1, 4)
+    return dateOnly.join(" ")
+  }
+
+
+	render() {
+    
+    return (
+	  	<div style={{marginTop: "100px"}}>
+        <h1>{this.state.topic}</h1>
+        {this.state.posts.map((post) => {
+          return (
+            <div className="wip-summary" key={post.id}>
+              <Link to={"/user/" + post.authorId}><div className="wip-logline-text">{post.author}</div></Link>
+              <div className="wip-logline-text">{this.simplifyDate(new Date(post.date).toUTCString())}</div>
+              <div className="wip-logline-text">{post.comment}</div>
+            </div>
+          )
+        })} 
+        <NewPostForm threadId={this.state.threadId} /> 
+      </div>
+	   );
+  }
+}
+
+export default Thread
